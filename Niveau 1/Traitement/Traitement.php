023df6eea6ba2dc1de913174d051824dac6b6616
Adjagarail <43035463+Adjagarail@users.noreply.github.com>
2018-09-21 16:10:06 +0000

    first commit

---
+++ Niveau 1/Traitement/Traitement.php
@@ -0,0 +1,7 @@
+<?php
+$table=$_POST['table'];
+  for ($i=0; $i <=10 ; $i++)
+  {
+      echo $i.'x'.$table.'='.$i*$table.'<br>';
+  }
+ ?>
---
+++ Niveau 3/index.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Apprendre ses tables de multiplication</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="Css/style.css">
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top menu">
+      <a class="navbar-brand" href="#">Table_Plus</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Acceuil<span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" href="Pages/level_1.php">Level 1</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" href="Pages/level_2.php">Level 2</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" href="Pages/quizz.php">Super quizz</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" href="Pages/quizz_2.php">Super Super Quizz</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <main role="main" class="container">
+
+      <div class="starter-template text-center">
+        <h1>Apprendre vos tables de multiplication en vous amusant</h1>
+        <p class="lead">Apprendre vos table de multiplication plus rapidement et profiter d'un suivit de votre apprentissage<br>Tous ceux dont vous avez besoin est là ajoutez y de la motivation et de la bonne volonté et tout ira pour le mieux</p>
+      </div>
+
+    </main>
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  </body>
+</html>
